4595184a899c39bea9540095b284aa31262c9c68
Ahsien <hsiaohsien0325@gmail.com>
2016-05-05 10:12:45 +0800

    "nettcpbinding" and call from "Channel".

---
+++ WcfLab_NetTcp/WcfLab.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Text;
+using System.Web.Script.Serialization;
+using System.ServiceModel;
+
+using WCFLab.Domains;
+using WCFLab.IService;
+
+namespace WCFLab.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var client = new HttpClient();
+            var serializer= new JavaScriptSerializer();
+
+            var getResult = client.GetAsync("http://localhost/WCFLabService/RestaurantService.svc/GetRestaurant?id=12").Result;
+
+            var restaurant = new Restaurant() { Id = 123, Name = "水餃王", Address = "你家旁邊", PhoneNumber = "23456789" };
+
+            var jsonString= serializer.Serialize(new { restaurant= restaurant });
+            var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+            var postResponse = client.PostAsync("http://localhost/WCFLabService/RestaurantService.svc/SaveRestaurant", content).Result;
+            var postContent = postResponse.Content.ReadAsStringAsync().Result;
+            var postResult = serializer.Deserialize<dynamic>(postContent)["SaveRestaurantResult"];
+            var newRestaurant = new Restaurant(postResult);
+
+
+            var factory = new ChannelFactory<IRestaurantService>("IRestaurantService");
+            var channel = factory.CreateChannel();
+            var resultFromTcpCall = channel.GetRestaurant(9);
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WcfLab_NetTcp/WcfLabService/RestaurantService.svc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+using WCFLab.Domains;
+using WCFLab.IService;
+
+namespace WCFLab.Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class RestaurantService :IRestaurantService
+    {
+        public Restaurant GetRestaurant(int id)
+        {
+            var result = new Restaurant { Id = id, Name = "水餃王", Address = "你家旁邊", PhoneNumber = "23456789" };
+            return result;
+        }
+
+        public Restaurant SaveRestaurant(Restaurant restaurant)
+        {
+            var restaurantq = new Restaurant { Id = 123, Name = "水餃王", Address = "你家旁邊", PhoneNumber = "23456789" };
+            return restaurant;
+        }
+    }
+    
+}
